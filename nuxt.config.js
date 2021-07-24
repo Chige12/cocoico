692762bbbe0201ddb263511ea987a33022b3573a
Ota1022 <ota.itaru.og4@is.naist.jp>
2021-07-24 21:59:01 +0900

    vuex-persistedstate

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '@/plugins/firebase'
+    { src: '@/plugins/firebase', ssr: false },
+    { src: '@/plugins/localStorage', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
